0f4b59aa09a33d0bea89161fb540eece5dd2522f
perfectdaemon <perfectdaemon@gmail.com>
2015-08-18 18:37:45 +0700

    clean of whitespace

--- Game/Interfaces/IController.cs
+++ Game/Interfaces/IController.cs
@@ -6,8 +6,8 @@ namespace SharpPixel
     interface IController
     {
         void SetRenderSurface(IRenderSurface surface);
-        void OnKeyDown(KeyEventArgs e);        
-        void Render();        
+        void OnKeyDown(KeyEventArgs e);
+        void Render();
         void Update(double dt);
 
         void SwitchTo(IScene scene);
